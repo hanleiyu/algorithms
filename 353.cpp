580c6d5adcbbdb96d729261a4952057bbb45ee6f
hanleiyu <hanleiyu@github.com>
2022-02-28 11:27:12 +0800

    28

---
+++ 353.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class SnakeGame {
+public:
+    SnakeGame(int width, int height, vector<vector<int>>& food) {
+        
+    }
+    
+    int move(string direction) {
+        
+    }
+};
+
+/**
+ * Your SnakeGame object will be instantiated and called as such:
+ * SnakeGame* obj = new SnakeGame(width, height, food);
+ * int param_1 = obj->move(direction);
+ */
